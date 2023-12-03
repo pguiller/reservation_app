f24bd9c90708863eff96bfa7ed012e6abe039562
Paul GUILLERMIER <pguillermier@iliad-free.fr>
2023-12-03 08:32:58 +0100

    back url conf

--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,3 @@
-export const BACK_URL = 'http://localhost:8000';
+export const BACK_URL = 'https://anniversaire-mariage-sp.fr/';
 export const ADRESS_EVENT =
   '2 Rue du Bourg Gelé, 89520 Saint-Sauveur-en-Puisaye';
